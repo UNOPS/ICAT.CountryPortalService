ae58d6d49d4f53699f6b63560f428047a9aa6a9d
Kashul <kashul@climatesi.com>
2022-09-22 14:26:02 +0530

    bugs

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -28,7 +28,7 @@ const config: ConnectionOptions = {
   // password: '12345',
   //database: 'nccdsndb',
   username: 'root',
-  password: 'pradeep123#',
+  password: '7860150',
   database: 'portelservice',
   // password: '12345',
   // database: 'portelservice',
