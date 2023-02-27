2517a9c37267238f28402d7d67d039bd9cd12fe5
Camila Haas Primieri <camilahp@unops.org>
2023-02-27 17:36:48 +0100

    Changed the node version to 18 and using env variables for the database

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -2,16 +2,11 @@ import { ConnectionOptions } from 'typeorm';
 
 const config: ConnectionOptions = {
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: 'password',
-  database: 'calculation',
-  // socketPath: process.env.SOCKET_PATH,
-  // port: Number(process.env.DATABASE_PORT),
-  // username: process.env.DATABASE_USER,
-  // password: process.env.DATABASE_PASSWORD,
-  // database: process.env.DATABASE_NAME,
+  socketPath: process.env.SOCKET_PATH,
+  port: Number(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
 
   synchronize: false,
