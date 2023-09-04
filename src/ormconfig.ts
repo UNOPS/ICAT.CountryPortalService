a7b63b2ac9b3619ebd4f7ae4388bddb2e02f9aea
Kashul <kashul@climatesi.com>
2023-09-04 11:28:03 +0530

    up

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -3,11 +3,16 @@ import { ConnectionOptions } from 'typeorm';
 const config: ConnectionOptions = {
   type: 'mysql',
   // host: process.env.DATABASE_HOST,
-  socketPath: process.env.SOCKET_PATH,
-  port: Number(process.env.DATABASE_PORT),
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
+  // socketPath: process.env.SOCKET_PATH,
+  // port: Number(process.env.DATABASE_PORT),
+  // username: process.env.DATABASE_USER,
+  // password: process.env.DATABASE_PASSWORD,
+  // database: process.env.DATABASE_NAME,
+
+    port: 3306,
+  username: "root",
+  password: "7860150",
+  database: "portelservice2", 
   
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   synchronize: false,
