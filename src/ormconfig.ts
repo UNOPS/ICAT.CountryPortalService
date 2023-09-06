e25ab0b50925c8e0e09f959f7c956de2e50d8bee
KashulClimateSI <135259896+KashulClimateSI@users.noreply.github.com>
2023-09-06 17:56:54 +0530

    Kh climate si (#7)

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -7,7 +7,7 @@ const config: ConnectionOptions = {
   port: Number(process.env.DATABASE_PORT),
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
+  database: process.env.DATABASE_NAME, 
   
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   synchronize: false,
